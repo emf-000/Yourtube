c76f2e839e25507f92331c8a847a446ef2d8033b
Mustafa Shaikh <mustafashaikh6634@gmail.com>
2025-11-28 20:52:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,12 @@ import planroutes from "./routes/plan.js";
 dotenv.config();
 const app = express();
 
+console.log("RENDER ENV:", {
+  host: process.env.SMTP_HOST,
+  user: process.env.SMTP_USER,
+  pass: process.env.SMTP_PASS ? "YES" : "NO"
+});
+
 app.use(cors());
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb" }));
@@ -32,8 +38,6 @@ app.use("/download", downloadroutes);
 app.use("/payment", paymentroutes);
 app.use("/plan", planroutes);
 
-
-
 // DEFAULT
 app.get("/", (req, res) => {
   res.send("Yourtube backend is working ✔✔");
@@ -48,6 +52,7 @@ mongoose
 // SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
 
 
