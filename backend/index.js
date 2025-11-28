d8459f04bf4de01dbcbed8f7891ddf61969db94d
Mustafa Shaikh <mustafashaikh6634@gmail.com>
2025-11-28 19:06:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,9 +33,14 @@ app.use("/download", downloadroutes);
 app.use("/payment", paymentroutes);
 app.use("/plan", planroutes);
 
-app.get("/test-route", (req, res) => {
-  res.send("TEST ROUTE WORKING");
+console.log("ENV CHECK:", {
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  user: process.env.SMTP_USER,
+  pass: process.env.SMTP_PASS ? "YES" : "NO",
+  from: process.env.EMAIL_FROM,
 });
+
 
 app.get("/debug-email", async (req, res) => {
   try {
@@ -77,4 +82,5 @@ mongoose
 // SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 
